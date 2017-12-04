648333bb106c7bcf993fbbd7fae5b0c87dfe641c
林周 <linz06@vanke.com>
2017-12-04 23:17:16 +0800

    add : ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,38 +3,38 @@ module.exports = {
    * Application configuration section
    * http://pm2.keymetrics.io/docs/usage/application-declaration/
    */
-  apps : [
+  apps: [
 
     // First application
     {
-      name      : 'rap2_back_end',
-      script    : 'dispatch.js',
+      name: 'rap2_back_end',
+      script: 'dispatch.js',
       env: {
         COMMON_VARIABLE: 'true'
       },
-      env_production : {
+      env_production: {
         NODE_ENV: 'production'
       }
-    },
+    }
   ],
 
   /**
    * Deployment section
    * http://pm2.keymetrics.io/docs/usage/deployment/
    */
-  deploy : {
-    production : {
-        user : 'joe',
-        host : '120.77.84.9',
-        ref  : 'origin/master',
-        repo : 'git@github.com:lzwebdev/rap2-delos.git',
-        path : '/home/joe/project/rap/rap2-delos',
-        port:'3999',
-        ssh_options:'StrictHostKeyChecking=no',
-        env: {
-            NODE_ENV: 'production'
-        },
-        'post-deploy' : 'npm install ;pm2 startOrRestart ecosystem.config.js --env production'
-    },
+  deploy: {
+    production: {
+      user: 'joe',
+      host: '120.77.84.9',
+      ref: 'origin/master',
+      repo: 'git@github.com:lzwebdev/rap2-delos.git',
+      path: '/home/joe/project/rap/rap2-delos',
+      port: '3999',
+      ssh_options: 'StrictHostKeyChecking=no',
+      env: {
+        NODE_ENV: 'production'
+      },
+      'post-deploy': 'npm install ;pm2 startOrRestart ecosystem.config.js --env production'
+    }
   }
-};
+}
